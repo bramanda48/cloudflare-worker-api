e6428f745976d3ba5d70f810f48286e7ca0e0cb0
Bramanda Suwandi <bramanda48@gmail.com>
2023-10-26 21:41:12 +0700

    chore: add database file

--- prisma/backup.sql
+++ prisma/backup.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Product` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
     `content` TEXT NULL,
-    `price` DECIMAL(65, 30) NOT NULL DEFAULT 0,
+    `price` DECIMAL(9, 2) NOT NULL DEFAULT 0,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NULL,
 
